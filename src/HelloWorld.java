41d704f190aa69de365e9c5fd8bdfee4c5258234
matthew jio <matthew.p.jio1@gmail.com>
2022-06-09 15:52:14 -0500

    variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,37 @@
+import java.sql.SQLOutput;
+
 public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello, Wooooooorld!");
+        int MyFavoriteNumber = 88;
+        System.out.println(MyFavoriteNumber);
+        String myString = "3.14159";
+        System.out.println(myString);
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+        //int x = 5;
+        //System.out.println(x++);
+        //System.out.println(x);
+        //int x = 5;
+        //System.out.println(++x);
+        //System.out.println(x);
+        //String class;
+        //String theNumberThree = "three";
+        //Object o = theNumberThree;
+        //int three = (int) o;
+        int x = 4;
+        x += 5;
+        System.out.println(x);
+
+        int a = 3;
+        int y = 4*a;
+        System.out.println(y);
+
+        int c = 10;
+        int d = 2;
+        c /= d;
+        d -= c;
+        System.out.println(c);
+        System.out.println(d);
     }
 }
