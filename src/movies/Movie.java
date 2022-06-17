7fd0724f224f6b11b6af9b292e8904b6310dc28e
matthew jio <matthew.p.jio1@gmail.com>
2022-06-17 16:51:39 -0500

    movies

---
+++ src/movies/Movie.java
@@ -0,0 +1,26 @@
+package movies;
+
+public class Movie {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    private String category;
+
+
+}
